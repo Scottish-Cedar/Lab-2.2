fda78b2d53c5df4ff17eb2991bb9f2c230c48237
fra99er2 <fra99er2@gmail.com>
2018-10-28 16:13:14 +0200

    Added improved output (int instead of string)

--- FastMul/fastmul.cpp
+++ FastMul/fastmul.cpp
@@ -2,21 +2,34 @@
 #include <iostream>
 using namespace std;
 #include <string>
-
+#include <cmath>
 
 class Z
 {
 private:
 	int mass[1000] = {};
+	int c1 = 0;
 public:
 	Z();
 	int input();
+	int getc1();
+	int getm(int i);
 	string output();
 };
 
 Z::Z()
 {
 
+}
+
+int Z::getc1()
+{
+	return c1;
+}
+
+int Z::getm(int i)
+{
+	return mass[i];
 }
 
 int Z::input()
@@ -57,7 +70,8 @@ int Z::input()
 			i++;
 		}
 	}
-	return i;
+	c1 = i;
+	return c1;
 }
 
 string Z::output()
@@ -78,6 +92,15 @@ int main()
 	string x1 = "";
 	Z Z1;
 	x1 = Z1.output();
+	long a = 0;
+	int i = 0;
+	while (i<Z1.getc1())
+	{
+		cout << Z1.getm(i) << endl;
+		a += Z1.getm(i) * pow(10, Z1.getc1()-i-2);
+		i++;
+	}
+	cout << a << endl;
 	cout << x1 << endl;
 	system("pause");
 	return 0;
